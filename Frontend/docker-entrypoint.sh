e9204a478408dd9c3bc218ddba33593683dee286
felixdeveloper87 <leandrofelix8787@gmail.com>
2025-01-30 18:01:15 +0000

    Corrigindo substituição de variáveis no nginx.conf

--- Frontend/docker-entrypoint.sh
+++ Frontend/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Substituir BACKEND_URL no nginx.conf
+# Define um valor padrão para ambiente local
+export BACKEND_URL="${BACKEND_URL:-http://localhost:5000}"
+
+# Substitui APENAS a variável BACKEND_URL no template
 envsubst '${BACKEND_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# Iniciar o Nginx
-exec "$@"
+# Inicia o Nginx (o comando padrão do Dockerfile)
+exec nginx -g 'daemon off;'
